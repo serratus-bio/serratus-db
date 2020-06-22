155efbd2f6d29b8e78564b7d90cdabbb1b26cfbe
lohrd <57124913+lohrd@users.noreply.github.com>
2020-06-22 15:43:41 -0400

    Update AccessionSection.cs

--- SerratusTest/Domain/Model/AccessionSection.cs
+++ SerratusTest/Domain/Model/AccessionSection.cs
@@ -10,12 +10,12 @@ namespace SerratusTest.Domain.Model
         public int AccessionSectionId { get; set; }
         public int AccessionSectionLineId { get; set; }
         public string Acc { get; set; }
-        public int PctId { get; set; }
+        public double PctId { get; set; }
         public int Aln { get; set; }
         public int Glb { get; set; }
         public int Len { get; set; }
-        public string CvgPct { get; set; }
-        public int Depth { get; set; }
+        public int CvgPct { get; set; }
+        public double Depth { get; set; }
         public string Cvg { get; set; }
         public string Fam { get; set; }
         public string Name { get; set; }
