94d092a3751bf0da645a3ad3b21883ac576c70be
lohrd <57124913+lohrd@users.noreply.github.com>
2020-06-22 13:56:30 -0400

    Create SerratusSummaryContext.cs

---
+++ SerratusTest/ORM/SerratusSummaryContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SerratusTest.Domain.Model;
+
+namespace SerratusTest.ORM
+{
+    public class SerratusSummaryContext : DbContext
+    {
+        public DbSet<CommentLine> CommentLines { get; set; }
+        public DbSet<FamilySection> FamilySections{ get; set; }
+        public DbSet<AccessionSection> AccessionSections { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseNpgsql("host=localhost;database=SerratusTest;username=postgres;password=bandit");
+    }
+}
