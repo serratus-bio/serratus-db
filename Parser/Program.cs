8267d8c3fc1b1ea51b6e7699aa28206ef4f430a9
lohrd <57124913+lohrd@users.noreply.github.com>
2020-06-25 15:39:48 -0400

    Run Parser

---
+++ Parser/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SerratusTest.ORM;
+using System;
+
+namespace ParserNs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parser = new Parser();
+            parser.ReadFile();
+            parser.ParseFile();
+            using (var context = new SerratusSummaryContext())
+            {
+                context.CommentLines.Add(parser.CommentLine);
+                context.SaveChanges();
+            }
+        }
+    }
+}
