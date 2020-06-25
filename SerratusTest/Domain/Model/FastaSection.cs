c0dc1464ea2a0b6849582fa5cfdea4721ecc628f
lohrd <57124913+lohrd@users.noreply.github.com>
2020-06-25 15:39:57 -0400

    Create FastaSection.cs

---
+++ SerratusTest/Domain/Model/FastaSection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SerratusTest.Domain.Model
+{
+    public class FastaSection
+    {
+        public int FastaSectionId { get; set; }
+        public int FastaSectionLineId { get; set; }
+        public string SequenceId { get; set; }
+        public string Sequence { get; set; }
+        public int CommentLineId { get; set; }
+    }
+}
