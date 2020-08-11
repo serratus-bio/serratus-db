9087ce7fc88ea1dc8d76869fd73fdbac5fa41a8f
lohrd <57124913+lohrd@users.noreply.github.com>
2020-08-11 02:15:52 -0400

    Create PaginatedResult.cs
    - Wrapper class for AccessionSections & FamilySections to include number of pages in API call

---
+++ SerratusApi/Model/PaginatedResult.cs
@@ -0,0 +1,15 @@
+﻿using SerratusTest.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SerratusApi.Model
+{
+    public class PaginatedResult
+    {
+        public IEnumerable<AccessionSection> AccessionSections { get; set; }
+        public IEnumerable<FamilySection> FamilySections { get; set; }
+        public int NumberOfPages { get; set; }
+    }
+}
