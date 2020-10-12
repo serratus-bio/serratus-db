55c5aa75786ebe5b4f9c44b2146e0b33870260fa
lohrd <57124913+lohrd@users.noreply.github.com>
2020-10-12 12:11:39 -0400

    add sequence table
    - from older accesions table

--- SerratusTest/Domain/Model/Sequence.cs
+++ SerratusTest/Domain/Model/Sequence.cs
@@ -5,23 +5,22 @@ using System.Threading.Tasks;
 
 namespace SerratusTest.Domain.Model
 {
-    public class Family
+    public class Sequence
     {
-        public int family_id { get; set; }
-        public int family_line { get; set; }
+        public int sequence_id { get; set; }
+        public int sequence_line { get; set; }
         public int run_id { get; set; }
-        public string sra_id { get; set; }
+        public int sra_id { get; set; }
+        public int family_id { get; set; }
+        public string genbank_id { get; set; }
+        public string genbank_name { get; set; }
         public string family_name { get; set; }
         public int score { get; set; }
-        public int percent_identity { get; set; }
+        public int percentage_identity { get; set; }
         public string coverage_bins { get; set; }
         public int n_reads { get; set; }
         public int n_global_reads { get; set; }
         public int length { get; set; }
         public double depth { get; set; }
-        public string top_genbank_id { get; set; }
-        public string top_name { get; set; }
-        public int top_score { get; set; }
-        public int top_length { get; set; }
     }
 }
